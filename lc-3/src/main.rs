b9bedcaf2df584f0d07088b21f8f7e6f1db3e8d0
jmpnz <dumpsterdev@gmail.com>
2023-05-22 20:12:26 -0400

    feat: more stuff on lc3

--- lc-3/src/main.rs
+++ lc-3/src/main.rs
@@ -1,6 +1,5 @@
 // Memory for LC-3 VM, has max size 65536 cells.
 const MEMORY_MAX: usize = 1 << 16;
-// static mut MEMORY: &'static mut [u16] = &mut [0; MEMORY_MAX];
 
 // Register definitions.
 #[derive(Copy, Clone, Debug)]
@@ -17,9 +16,6 @@ enum Register {
     Cond,
     Count,
 }
-
-// Registers.
-static mut REGISTERS: &'static mut [u16] = &mut [0; Register::Count as usize];
 
 // Opcodes
 #[derive(Copy, Clone, Debug)]
@@ -96,6 +92,26 @@ impl VirtualMachine {
     pub fn read(&self, pos: usize) -> u16 {
         self.memory[pos]
     }
+    // Update condition flags on each register write.
+    pub fn update_flags(&mut self, r: u16) {
+        if self.registers[r as usize] == 0 {
+            self.registers[Register::Cond as usize] = CondFlags::Zero as u16;
+        } else if (self.registers[r as usize] >> 15) != 0 {
+            self.registers[Register::Cond as usize] = CondFlags::Neg as u16;
+        } else {
+            self.registers[Register::Cond as usize] = CondFlags::Pos as u16;
+        }
+    }
+}
+
+// Sign extension for immediate values, sign extension is used
+// to extend values stored in n-bits to m-bits (m > n) and also
+// preserve their sign.
+fn sext(x: u16, bit_count: usize) -> u16 {
+    if ((x >> (bit_count - 1)) & 1) != 0 {
+        return x | (0xFFFF << bit_count);
+    }
+    x
 }
 
 fn main() {
@@ -107,14 +123,30 @@ fn main() {
     vm.registers[Register::Pc as usize] = 0x3000;
     vm.memory[0x3000] = OPCode::Add as u16;
     loop {
+        // Offset of the next instruction.
         let offset = vm.registers[Register::Pc as usize];
-        println!("Offset : {:#0x}", offset);
+        // Fetch the next instruction.
         let inst = vm.read(offset as usize);
+        // Decode the next instruction.
+        let op = inst >> 12;
 
         match OPCode::get(inst) {
-            Some(OPCode::Add) => println!("OPAdd"),
+            Some(OPCode::Add) => println!("Add"),
+            Some(OPCode::And) => println!("And"),
+            Some(OPCode::Not) => println!("Not"),
+            Some(OPCode::Br) => println!("Br"),
+            Some(OPCode::Jmp) => println!("Jmp"),
+            Some(OPCode::Jsr) => println!("Jsr"),
+            Some(OPCode::Ld) => println!("Ld"),
+            Some(OPCode::Ldi) => println!("Ldi"),
+            Some(OPCode::Ldr) => println!("Ldr"),
+            Some(OPCode::Lea) => println!("Lea"),
+            Some(OPCode::St) => println!("St"),
+            Some(OPCode::Sti) => println!("Sti"),
+            Some(OPCode::Str) => println!("Str"),
+            Some(OPCode::Trap) => println!("Trap"),
+            Some(OPCode::Res) | Some(OPCode::Rti) => println!("Riti"),
             None => println!("Unknown instruction"),
-            _ => println!("Misaligned instruction or code"),
         }
         break;
     }
